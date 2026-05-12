ceb132d6a7c0ed12c0a0830c3e98bff2b54126d1
David-347 <152646281+David-347@users.noreply.github.com>
2026-05-12 09:24:31 -0600

    Add files via upload

---
+++ main/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'srig-v3-cache-v1';
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/app.js",
+  "/js/websocket.js",
+  "/js/charts.js",
+  "/js/i18n.js",
+  "/manifest.json",
+  "/img/logo-srig.png",
+  "/img/light-theme.png",
+  "/img/dark-theme.png"
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
